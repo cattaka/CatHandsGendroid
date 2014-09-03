154f7270134a0a5b54aa2459516fcd88f1e1e73b
cattaka <cattaka@mail.cattaka.net>
2014-09-04 06:21:37 +0900

    Fix "put @Attribute(primaryKey=true)" error when genParcelFunc=true, genDbFunc=false.

--- cathandsgendroid-apt/src/test/java/net/cattaka/util/cathandsgendroid/test/model/TinyParcelable.java
+++ cathandsgendroid-apt/src/test/java/net/cattaka/util/cathandsgendroid/test/model/TinyParcelable.java
@@ -1,50 +1,40 @@
 
 package net.cattaka.util.cathandsgendroid.test.model;
 
+import net.cattaka.util.cathandsgendroid.annotation.DataModel;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+@DataModel(genParcelFunc=true, genDbFunc=false)
 public class TinyParcelable implements Parcelable {
-    private int mData;
-
+    public static final android.os.Parcelable.Creator<TinyParcelable> CREATOR = TinyParcelableCatHands.CREATOR;
+    
     public int describeContents() {
         return 0;
     }
 
     public void writeToParcel(Parcel out, int flags) {
-        out.writeInt(mData);
+        TinyParcelableCatHands.writeToParcel(this, out, flags);
     }
 
-    public static final Parcelable.Creator<TinyParcelable> CREATOR = new Parcelable.Creator<TinyParcelable>() {
-        public TinyParcelable createFromParcel(Parcel in) {
-            return new TinyParcelable(in);
-        }
-
-        public TinyParcelable[] newArray(int size) {
-            return new TinyParcelable[size];
-        }
-    };
-
-    private TinyParcelable(Parcel in) {
-        mData = in.readInt();
-    }
+    private int data;
 
     public TinyParcelable() {
     }
 
     
     
-    public TinyParcelable(int mData) {
+    public TinyParcelable(int data) {
 		super();
-		this.mData = mData;
+		this.data = data;
 	}
 
 	public int getData() {
-        return mData;
+        return data;
     }
 
     public void setData(int data) {
-        this.mData = data;
+        this.data = data;
     }
 
 }
