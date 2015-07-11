7f567a4448092cd3541e0dd54109008a65c525e2
cattaka <cattaka@mail.cattaka.net>
2015-07-11 19:25:09 +0900

    Support generics argments

--- cathandsgendroid-apt/src/test/java/net/cattaka/util/cathandsgendroid/test/asyncif/SimpleInterface.java
+++ cathandsgendroid-apt/src/test/java/net/cattaka/util/cathandsgendroid/test/asyncif/SimpleInterface.java
@@ -1,9 +1,13 @@
 package net.cattaka.util.cathandsgendroid.test.asyncif;
 
+
+import java.util.List;
+
 import net.cattaka.util.cathandsgendroid.annotation.AsyncInterface;
 
 @AsyncInterface
 public interface SimpleInterface {
     public void run();
     public int add(int a, int b);
+    public List<Integer> runLists(List<Integer> args);
 }
